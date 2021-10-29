81468fe9f41f3cc93f5cfc9b6faaa10468c95a3b
Andrii Kovalchuk <kovalchukaiwe@gmail.com>
2021-10-29 22:46:59 +0300

    feat(src): created components, made some markup

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import Logo from '../Logo/Logo';
+import styles from './Footer.module.scss';
+
+// eslint-disable-next-line arrow-body-style
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer__links}>
+        <a className={styles.footer__item} href='#'>
+          Home
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Home
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Ecosystem
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Blocks
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Charts
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Home
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Bakers
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Assets
+        </a>
+        <a className={styles.footer__item} href='#'>
+          Home
+        </a>
+      </div>
+      <div className={styles.footer__rights}>
+        <p>@ Copyright. Company name. 2021</p>
+      </div>
+      <div className={styles.footer__logo}>
+        <Logo />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import styles from './Table.module.scss';
+
+const Table = () => {
+  return (
+    <div className={styles.table}>
+      data
+    </div>
+  );
+}
+
+export default Table;
