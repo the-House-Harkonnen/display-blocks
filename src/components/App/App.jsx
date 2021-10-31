375bcda2d2dbc467082fe64183491f92a431b316
Vovchanchyk <oberigfast@gmail.com>
2021-10-31 15:16:00 +0200

    feat(app): add basic structure of routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../Header/Header';
 import Main from '../Main';
 
 export const App = () => (
   <div>
-    <Header />
-    <Main />
-    <Footer />
+    <Router>
+      <Header />
+      <Main />
+      <Footer />
+    </Router>
   </div>
 );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { Block } from '../../pages/Block';
 import { Blocks } from '../../pages/Blocks';
 
@@ -8,8 +9,17 @@ import styles from './Main.module.scss';
 const Main = () => {
   return (
     <div className={styles.main}>
-      <Blocks />
-      <Block />
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/blocks' />
+        </Route>
+        <Route exact path='/blocks'>
+          <Blocks />
+        </Route>
+        <Route exact path='/blocks/:blockId'>
+          <Block />
+        </Route>
+      </Switch>
     </div>
   );
 };
