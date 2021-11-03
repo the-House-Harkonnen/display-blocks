1a1c5f9c86cf4b0e1dffc112eb480070009531db
Vovchanchyk <60846596+vovchanchyk@users.noreply.github.com>
2021-11-03 19:59:07 +0200

    feat(nav):  adaptive navigation (#11)
    * fix(nav): fix navigation
    
    * fix(remove btn conditionaly): remove btn conditionaly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,35 @@
+/* eslint-disable no-console */
 /* eslint-disable arrow-body-style */
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useState } from 'react';
+// import { Burger } from '../Burger';
 import LoginBtn from '../LoginBtn';
 import Logo from '../Logo';
 import Nav from '../Nav';
 import styles from './Header.module.scss';
+import burgerIcon from "../../imgs/Vector.svg";
+import userIcon from "../../imgs/Group.svg";
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false)
+  console.log(showNav);
+  const classNav = showNav? `${styles.nav} ${styles.show}`: styles.nav;
   return (
     <div className={styles.header}>
       <Logo />
+      <div className={styles.burger}>
+      <button type='button'>
+      <img src={userIcon} alt="burger" />
+      </button>
+      <button type='button' onClick={()=> setShowNav(!showNav)}>
+      <img src={burgerIcon} alt="burger" />
+      </button>
+      </div>
+      <div className={classNav}> 
       <Nav />
       <LoginBtn />
+      </div>   
+      
     </div>
   );
 }
