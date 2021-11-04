48483523cfa9f81039de98a50e566a699184c9f8
Andrii Kovalchuk <kovalchukaiwe@gmail.com>
2021-11-04 18:40:02 +0200

    fix(heder/footer): styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,33 +8,39 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.footer__links}>
-        <a className={styles.footer__item} href='#'>
-          Home
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Home
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Ecosystem
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Blocks
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Charts
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Home
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Bakers
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Assets
-        </a>
-        <a className={styles.footer__item} href='#'>
-          Home
-        </a>
+        <div className={styles.footer__group}>
+          <a className={styles.footer__item} href='#'>
+            Home
+          </a>
+          <a className={styles.footer__item} href='#'>
+            Blocks
+          </a>
+          <a className={styles.footer__item} href='#'>
+            Bakers
+          </a>
+        </div>
+        <div className={styles.footer__group}>
+          <a className={styles.footer__item} href='#'>
+            Home
+          </a>
+          <a className={styles.footer__item} href='#'>
+            Charts
+          </a>
+          <a className={styles.footer__item} href='#'>
+            Assets
+          </a>
+        </div>
+        <div className={styles.footer__group}>
+          <a className={styles.footer__item} href='#'>
+            Ecosystem
+          </a>
+          <a className={styles.footer__item} href='#'>
+            Home
+          </a>
+          <a className={styles.footer__item} href='#'>
+            Home
+          </a>
+        </div>
       </div>
       <div className={styles.footer__rights}>
         <p>@ Copyright. Company name. 2021</p>
