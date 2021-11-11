d0e28fd2b6999f316d06cf3a7ce5ea75c55a6464
Vovchanchyk <oberigfast@gmail.com>
2021-11-11 16:51:26 +0200

    fix(header): fix nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-console */
-/* eslint-disable arrow-body-style */
-/* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
 import LoginBtn from '../LoginBtn';
 import Logo from '../Logo';
@@ -9,26 +6,26 @@ import styles from './Header.module.scss';
 import { BurgerIcon, UserIcon } from '../Icons/Icons';
 
 const Header = () => {
-  const [showNav, setShowNav] = useState(false)
-  const classNav = showNav? `${styles.nav} ${styles.show}`: styles.nav;
+  const [showNav, setShowNav] = useState(false);
+  const classNav = showNav ? `${styles.nav} ${styles.show}` : styles.nav;
+
   return (
     <div className={styles.header}>
       <Logo />
       <div className={styles.burger}>
-      <button type='button'>
-      <UserIcon />
-      </button>
-      <button type='button' onClick={()=> setShowNav(!showNav)}>
-      <BurgerIcon />
-      </button>
+        <button type='button'>
+          <UserIcon />
+        </button>
+        <button type='button' onClick={() => setShowNav(!showNav)}>
+          <BurgerIcon />
+        </button>
       </div>
-      <div className={classNav}> 
-      <Nav />
-      <LoginBtn />
-      </div>   
-      
+      <div className={classNav}>
+        <Nav />
+        <LoginBtn />
+      </div>
     </div>
   );
-}
+};
 
 export default Header;
