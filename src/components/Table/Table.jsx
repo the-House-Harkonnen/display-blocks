d4333ba20dd08a68455412633f6939c10bc2fc4c
Vovchanchyk <oberigfast@gmail.com>
2021-11-13 18:03:28 +0200

    feat(table): table in process

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,18 +1,45 @@
+/* eslint-disable consistent-return */
+/* eslint-disable prettier/prettier */
+/* eslint-disable guard-for-in */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-shadow */
+/* eslint-disable array-callback-return */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Table.module.scss';
 
-export const Table = ({ children }) => (
-  <div className={styles.container}>
-    <table className={styles.table}>
-      <thead>
-        <tr>{children[0]}</tr>
-      </thead>
-      <tbody>{children[1]}</tbody>
-    </table>
-  </div>
-);
+export const Table = ({ head, body }) => {
 
-Table.propTypes = {
-  children: PropTypes.node.isRequired,
+
+
+  console.log(body[0], head);
+
+  const row = []
+  for (const iterator in head) {
+    const {key} = head[iterator];
+
+    console.log(body[0][key])
+    row.push(body[0][key])
+
+  }
+  console.log(row)
+
+
+  return (
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {head.map((el) => (
+              <th key={el.name}>{el.name}</th>
+            ))}
+          </tr>
+        </thead>
+        {/* <tbody /> */}
+      </table>
+    </div>
+  );
 };
--- src/pages/Blocks/Blocks.jsx
+++ src/pages/Blocks/Blocks.jsx
@@ -1,19 +1,56 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useReducer } from 'react';
-import { BlocksBody } from '../../components/BlocksBody';
-import { BlocksHead } from '../../components/BlocksHead';
 import { BlocksPagination } from '../../components/BlocksPagination/BlocksPagination';
 import { Crumbs } from '../../components/Crumbs';
 import { Spinner } from '../../components/Spinner';
 import { Table } from '../../components/Table';
 import { useBlocksContext } from '../../context/blocksContext';
 import styles from './Blocks.module.scss';
-import { sortKeys } from './utils/sortConfig';
 import sortReducer from './utils/sortReducer';
+import { CellLinkIcon, CellLinkOption } from '../../components/Cell';
 
 export const Blocks = () => {
   const [sort, sortDispatch] = useReducer(sortReducer, { inc: true, key: '' });
   const sortHandler = (val) => sortDispatch(val);
+
+  const headConfig = [
+    {
+      name: 'Block ID',
+      key: 'level',
+      process: (value) => <CellLinkOption cell={value} />,
+    },
+    {
+      name: 'Created',
+      key: 'timestamp',
+      process: (value) => <td>{value}</td>,
+    },
+    {
+      name: 'Baker',
+      key: 'bakerName',
+      process: (value) => <td>{value}</td>,
+    },
+    {
+      name: 'Priority',
+      key: 'priority',
+      process: (value) => <td>{value}</td>,
+    },
+    {
+      name: '# of operations',
+      key: 'number_of_operations',
+      process: (value) => <td>{value}</td>,
+    },
+    {
+      name: 'Volume',
+      key: 'volume',
+      process: (value) => <td>{inT(value)}</td>,
+    },
+    {
+      name: '# of endorsements',
+      key: 'endorsements',
+      process: (value) => <td>{inTezosHandler(value)}</td>,
+    },
+  ];
 
   const { blocks, isFetching } = useBlocksContext();
   if (isFetching) return <Spinner />;
@@ -24,14 +61,7 @@ export const Blocks = () => {
       <h2 className={styles.title}>Blocks</h2>
       <div className={styles.list}>
         <div className={styles.table}>
-          <Table>
-            <BlocksHead
-              sort={sort}
-              callback={sortHandler}
-              sortKeys={sortKeys}
-            />
-            <BlocksBody sort={sort} />
-          </Table>
+          <Table head={headConfig} body={blocks} />
         </div>
         <div className={styles.pagination}>
           <BlocksPagination />
