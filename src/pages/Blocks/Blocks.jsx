bb3edd7da0e58c02f096ddb34c4192857a72878b
Vovchanchyk <oberigfast@gmail.com>
2021-11-16 15:29:35 +0200

    TE-1-refactor/react-table

--- src/pages/Blocks/Blocks.jsx
+++ src/pages/Blocks/Blocks.jsx
@@ -1,14 +1,14 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-import React, { useReducer } from 'react';
-import { BlocksBody } from '../../components/BlocksBody';
-import { BlocksHead } from '../../components/BlocksHead';
+import React, { useMemo, useReducer } from 'react';
+import { useTable, useSortBy } from 'react-table';
 import { BlocksPagination } from '../../components/BlocksPagination/BlocksPagination';
 import { Crumbs } from '../../components/Crumbs';
 import { Spinner } from '../../components/Spinner';
-import { Table } from '../../components/Table';
 import { useBlocksContext } from '../../context/blocksContext';
 import styles from './Blocks.module.scss';
-import { sortKeys } from './utils/sortConfig';
 import sortReducer from './utils/sortReducer';
 
 export const Blocks = () => {
@@ -16,6 +16,53 @@ export const Blocks = () => {
   const sortHandler = (val) => sortDispatch(val);
 
   const { blocks, isFetching } = useBlocksContext();
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Block ID',
+        accessor: 'level',
+      },
+      {
+        Header: 'Created',
+        accessor: 'timestamp',
+      },
+      {
+        Header: 'Baker',
+        accessor: 'bakerName',
+      },
+      {
+        Header: 'Priority',
+        accessor: 'priority',
+      },
+      {
+        Header: '# of operations',
+        accessor: 'number_of_operations',
+      },
+      {
+        Header: 'Volume',
+        accessor: 'volume',
+      },
+      {
+        Header: '# of endorsements',
+        accessor: 'endorsements',
+      },
+    ],
+    [blocks],
+  );
+  const data = useMemo(() => blocks, [blocks]);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy,
+    );
+  // in table getTableProps for table tag;
+  // {...column.getHeaderProps(  )}
+
   if (isFetching) return <Spinner />;
 
   return (
@@ -23,10 +70,48 @@ export const Blocks = () => {
       <Crumbs />
       <h2 className={styles.title}>Blocks</h2>
       <div className={styles.list}>
-        <Table>
-          <BlocksHead sort={sort} callback={sortHandler} sortKeys={sortKeys} />
-          <BlocksBody sort={sort} />
-        </Table>
+        <div className={styles.table}>
+          <table {...getTableProps()} className={styles.table}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => {
+                    return (
+                      <th
+                        {...column.getHeaderProps(
+                          column.getSortByToggleProps(),
+                        )}
+                      >
+                        {column.render('Header')}
+                        <span>
+                          {column.isSorted
+                            ? column.isSortedDec
+                              ? 'ᐯ'
+                              : 'ᐱ'
+                            : ' '}
+                        </span>
+                      </th>
+                    );
+                  })}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
         <div className={styles.pagination}>
           <BlocksPagination />
         </div>
