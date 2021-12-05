118230ca38bae99d30b08c81231f8d1eaac823da
Vovchanchyk <60846596+vovchanchyk@users.noreply.github.com>
2021-12-05 15:27:04 +0200

    Refactor/api (#82)
    * feat(contexts): add API context

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,9 +4,19 @@ import { useCallback } from 'react/cjs/react.development';
 import { UserIcon, BurgerIcon } from '../Icons/Icons';
 import { useThemeContext } from '../../contexts/themeContext';
 import styles from './Nav.module.scss';
+<<<<<<< HEAD
 import NavLink from '../NavLink/NavLink';
 
 const Nav = () => {
+=======
+import { DropDown } from '../DropDown/DropDown';
+import { useApiContext } from '../../contexts/apiContexts';
+
+const Nav = () => {
+  const { network, networkList, handleNetwork } = useApiContext();
+  const options = networkList.map((el) => el.value);
+
+>>>>>>> Refactor/api (#82)
   const history = useHistory();
   const [{ theme }] = useThemeContext();
   const [showNav, setShowNav] = useState(false);
@@ -54,8 +64,97 @@ const Nav = () => {
             color: theme.color,
           }}
         >
+<<<<<<< HEAD
           <NavLink />
           <span
+=======
+          <ul className={styles.links__list}>
+            <li className={styles.links__item}>
+              <a className={styles.links__link} href='#'>
+                Home
+              </a>
+            </li>
+            <li className={styles.links__item}>
+              <a className={styles.links__link} href='#'>
+                Blocks
+              </a>
+              <div className={styles.links__arrow} />
+            </li>
+            <li className={styles.links__item}>
+              <DropDown
+                name={network}
+                options={options}
+                callBack={handleNetwork}
+              />
+            </li>
+            <li className={styles.links__item}>
+              <a className={styles.links__link} href='#'>
+                Charts
+              </a>
+            </li>
+          </ul>
+          <ul
+            className={styles.links__burger}
+            style={{
+              backgroundColor: theme.burgerBackground,
+            }}
+          >
+            <li className={styles.links__item}>
+              <div className={styles.links__icon}>
+                <HomeIcon />
+              </div>
+              <a className={styles.links__link} href='#'>
+                Home
+              </a>
+            </li>
+            <li className={styles.links__item}>
+              <div className={styles.links__icon}>
+                <BlocksIcon />
+              </div>
+              <a className={styles.links__link} href='#'>
+                Blocks
+              </a>
+              <div className={styles.links__arrow} />
+            </li>
+            <li className={styles.links__item}>
+              <div className={styles.links__icon}>
+                <BakersIcon />
+              </div>
+              <a className={styles.links__link} href='#'>
+                Bakers
+              </a>
+              <div className={styles.links__arrow} />
+            </li>
+            <li className={styles.links__item}>
+              <div className={styles.links__icon}>
+                <ChartsIcon />
+              </div>
+              <a className={styles.links__link} href='#'>
+                Charts
+              </a>
+            </li>
+            <li className={styles.links__item}>
+              <div className={styles.links__icon}>
+                <EcosystemIcon />
+              </div>
+              <a className={styles.links__link} href='#'>
+                Ecosystem
+              </a>
+              <div className={styles.links__arrow} />
+            </li>
+            <li className={styles.links__item}>
+              <div className={styles.links__icon}>
+                <EcosystemIcon />
+              </div>
+              <DropDown
+                name={network}
+                options={options}
+                callBack={handleNetwork}
+              />
+            </li>
+          </ul>
+          <div
+>>>>>>> Refactor/api (#82)
             className={styles.line}
             style={{
               backgroundColor: theme.burgerMenuLine,
