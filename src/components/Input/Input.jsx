29c0abcbaa67d3381d67e32e511af9f965ce6b70
Vovchanchyk <oberigfast@gmail.com>
2021-12-10 10:57:46 +0200

    fix(input): add prop-types, delete comments

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,12 +1,10 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-console */
-/* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from 'prop-types';
 
 import React from 'react';
 import styles from './Input.module.scss';
 
-export const Input = ({ placeholder, name, value, onChange, onBlur, type }) => {
+export const Input = ({ placeholder, field, type }) => {
+  const { name, value, onChange, onBlur } = field;
   return (
     <input
       type={type}
@@ -22,18 +20,22 @@ export const Input = ({ placeholder, name, value, onChange, onBlur, type }) => {
 };
 
 Input.propTypes = {
-  name: PropTypes.string,
-  value: PropTypes.string,
+  field: PropTypes.shape({
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    onBlur: PropTypes.func,
+  }),
   type: PropTypes.string,
-  onChange: PropTypes.func,
-  onBlur: PropTypes.func,
   placeholder: PropTypes.string.isRequired,
 };
 
 Input.defaultProps = {
-  name: '',
-  value: '',
   type: 'text',
-  onChange: () => null,
-  onBlur: () => null,
+  field: {
+    name: '',
+    value: '',
+    onChange: () => null,
+    onBlur: () => null,
+  },
 };
